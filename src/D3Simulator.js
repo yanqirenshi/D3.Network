6e49b5e03e4d33876f14fb8ac4b49243a42006da
yanqirenshi <yanqirenshi@gmail.com>
2018-10-09 09:58:19 +0900

    add chage in D3Simulator

--- src/D3Simulator.js
+++ src/D3Simulator.js
@@ -1,12 +1,18 @@
 class D3Simulator {
     constructor(reducer) {}
     make() {
-        return d3.forceSimulation()
+        let simulation = d3.forceSimulation()
             .force("link", d3.forceLink().id(function(d) { return d._id; }))
             .force("charge", d3.forceManyBody())
-            .force("center", d3.forceCenter(0, 0))
+            //.force("center", d3.forceCenter(0, 0))
             .alphaMin(0.001)
             .alphaTarget(0.002)
-            .force("collide", d3.forceCollide(22));
+            .force("collide", d3.forceCollide(88));
+
+        simulation.force("charge")
+            .strength(function() { return -0.8; });
+
+        return simulation;
+
     }
 }
