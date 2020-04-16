fc1845451729b4085e580f686092da20ec14ad43
岩崎仁是 <yanqirenshi@gmail.com>
2020-04-16 15:49:52 +0900

    fix import and export

--- react/index.js
+++ react/index.js
@@ -1,6 +1,12 @@
-export { D3NetworkNode }       from './src/D3NetworkNode.js';
-export { D3NetworkEdge }       from './src/D3NetworkEdge.js';
-export { D3NetworkSimulation } from './src/D3NetworkSimulation.js';
-import D3Network               from './src/D3Network.js';
+import D3NetworkNode from './src/D3NetworkNode.js';
+import D3NetworkEdge from './src/D3NetworkEdge.js';
+import D3NetworkSimulation from './src/D3NetworkSimulation.js';
+import D3Network from './src/D3Network.js';
+
+export {
+    D3NetworkNode,
+    D3NetworkEdge,
+    D3NetworkSimulation
+};
 
 export default D3Network;
