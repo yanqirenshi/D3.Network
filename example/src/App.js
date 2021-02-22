bf849aa7933aca601755e9e69950be9f674a0288
yanqirenshi <yanqirenshi@gmail.com>
2021-02-22 12:32:02 +0900

    fix example

--- example/src/App.js
+++ example/src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import './App.css';
+import D3Network, {Camera} from '@yanqirenshi/d3.network';
 
-import PageExample from './components/PageExample';
+import ExampleData from './data/ExampleData.js';
 
-function App() {
-    const style = {
-        root: {
-            background: '#fefefe',
+export default function App() {
+    const [exdata] = useState(new ExampleData());
+    const [graph_data, setGraphData] = useState({
+        nodes: exdata.makeData('nodes'),
+        edges: exdata.makeData('edges'),
+    });
+    const [camera] = useState(new Camera({
+        look: {
+            at: {x:0, y:0},
+            scale: 1,
         },
-    };
+    }));
 
     return (
-        <div className="App" style={style.root}>
-          <PageExample />
+        <div style={{padding: 22}}>
+          <div style={{height:555, border: '1px solid #eee'}}>
+            <D3Network source={graph_data} camera={camera} />
+          </div>
         </div>
     );
 }
-
-export default App;
